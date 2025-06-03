4dad3dc1c533ad84f107a42b3c6fcd5a4f54436c
JW-Albert <ru04jo30801@gmail.com>
2025-06-03 23:08:10 +0800

    Add internationalization support with English and Chinese languages
    Implement i18n features by enabling language switching, translating UI elements, and configuring localization settings. This enhances user accessibility by allowing seamless language transitions between English and Traditional Chinese.

--- friends/urls.py
+++ friends/urls.py
@@ -19,8 +19,14 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.conf.urls.i18n import i18n_patterns
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("", include("episode.urls")),
+    path('i18n/', include('django.conf.urls.i18n')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+urlpatterns += i18n_patterns(
+    path("", include("episode.urls")),
+    prefix_default_language=False
+)
